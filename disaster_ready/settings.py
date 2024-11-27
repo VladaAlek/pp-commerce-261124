ab08396d7b4d262a3d0bc4487a43fad827476be2
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-11-27 11:03:13 +0000

    set allauth

--- disaster_ready/settings.py
+++ disaster_ready/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    #'allauth.socialaccount',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +82,16 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'disaster_ready.wsgi.application'
 
