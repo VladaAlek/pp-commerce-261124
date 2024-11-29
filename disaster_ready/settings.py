adfa6e1a1a7669b6412409961c2dfd794cdbeed2
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-11-29 13:52:58 +0000

    feat: set base.html and home page, navbar - logo, links, accounts managament and shopping basket

--- disaster_ready/settings.py
+++ disaster_ready/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=2fh2i7ev2vr&ax7t%pkbqg+4f$=qq81ymjgf$!6i1_oy9j7)%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-vladaalek-ppcommerce261-ou079ktz3ml.ws-eu116.gitpod.io']
+ALLOWED_HOSTS = ['8000-vladaalek-ppcommerce261-ou079ktz3ml.ws-eu117.gitpod.io']
 
 
 # Application definition
@@ -148,6 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
