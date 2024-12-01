111c7e216675818c891845d9deadd5a116e7885a
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-12-01 18:18:13 +0000

    add verbose name to categories

--- products/models.py
+++ products/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
+class Meta:
+        verbose_name_plural = 'Categories'
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     friendly_name = models.CharField(max_length=100)
