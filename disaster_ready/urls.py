0f3cb75b8a232eafe1eee0f46bdb82e2da2a4c33
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-12-06 09:41:39 +0000

    refact: deactivate 404 page and reload data in categories.json

--- disaster_ready/urls.py
+++ disaster_ready/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+#from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +26,4 @@ urlpatterns = [
     path('categories/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+#handler404 = 'boutique_ado.views.handler404'
