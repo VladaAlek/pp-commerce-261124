b64ceadf4904b9f0259866eaf4c69cb180329e64
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-12-08 16:38:38 +0000

    feat: basic setup for the bag app

--- disaster_ready/urls.py
+++ disaster_ready/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('categories/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #handler404 = 'boutique_ado.views.handler404'
--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.all_categories, name='categories'),
     path('<int:category_id>/', views.category_detail, name='materials'),
+    #path('products/', views.products, name='products'),
 ]
