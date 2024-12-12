eef63fea045764deef727ae1ffd6a6c8cdba4ef4
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-12-12 17:06:16 +0000

    refactor contexts.py and bag.html to show discount and discount_delta

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,30 +11,33 @@ def bag_contents(request):
     discount_delta = 0
     bag = request.session.get('bag', {})
 
-
     for item_id, quantity in bag.items():
         course = get_object_or_404(Category, pk=item_id)
-        total += course.price
-        product_count += 1
+        total += course.price * quantity
+        product_count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': course,
-    })
+        })
 
-    
     if total > settings.DISCOUNT_TRESHOLD:
         discount = total * Decimal(settings.STANDART_DISCOUNT_PERCENTAGE / 100)
         discounted_total = total - discount
-        discount_delta = total - discount_delta
+        grand_total = discounted_total
     else:
         discounted_total = total
+        grand_total = total
+        discount_delta = settings.DISCOUNT_TRESHOLD - total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'discount': discount,
+        'discounted_total': discounted_total,
+        'grand_total': grand_total,
+        'discount_delta': discount_delta,
         'STANDART_DISCOUNT_PERCENTAGE': settings.STANDART_DISCOUNT_PERCENTAGE,
         'DISCOUNT_TRESHOLD': settings.DISCOUNT_TRESHOLD,
     }
