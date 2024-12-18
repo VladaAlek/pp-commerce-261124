f615005e07d863f06f7467b5efff0fbc40a37b20
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-12-18 14:33:29 +0000

    add messages to add and remove views in bag app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse, reverse
+from django.shortcuts import render, redirect, HttpResponse, reverse, get_object_or_404
 from django.contrib import messages
 from products.models import Category
 
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified course to the shopping bag """
 
-    category = Category.objects.get(pk=item_id)
+    category = get_object_or_404(Category, pk= item_id)
     quantity = int(request.POST.get('quantity', 1)) 
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -30,13 +30,17 @@ def add_to_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+    category = get_object_or_404(Category, pk= item_id)
     try:
         bag = request.session.get('bag', {})
         if item_id in bag:
             bag.pop(item_id)
             request.session['bag'] = bag
+            messages.warning(request, f'You removed {category.name} course from your bag')
+            
            
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing category {e} from your bag')
         print(e)
         return HttpResponse(status=500)
