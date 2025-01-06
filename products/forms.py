74927c39b67fdabf136d5e83019ade58435b2435
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-01-06 14:44:38 +0000

    add widgets to template

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Category, Material
 
 
@@ -7,6 +8,8 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
