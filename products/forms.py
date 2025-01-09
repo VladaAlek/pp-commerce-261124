2fa4d5cac130e1939377a376710d601ad28e50ab
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-01-09 20:32:19 +0000

    refact: delete image value in products/forms

--- products/forms.py
+++ products/forms.py
@@ -9,7 +9,6 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
