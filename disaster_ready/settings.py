f1f5a2bdac10da42ef3e58cdb90b4a1eb66693b1
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-02-11 14:57:18 +0000

    deploy project to be debugged 2

--- disaster_ready/settings.py
+++ disaster_ready/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+
+# 'DEVELOPMENT' in os.environ
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-coral-harrier-tfa79yqc0yt.ws-eu117.gitpod.io',
